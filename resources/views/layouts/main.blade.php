0691d240dce2e4f0eb0b6000b7c945ca1d5082ca
Hapisss <alfa.ozora89@gmail.com>
2023-03-29 09:42:19 +0700

    29/03/23

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>SPK BLT</title>
+    <title>SPK BANSOS</title>
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset ('css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset ('css/datepicker3.css')}}" rel="stylesheet">
